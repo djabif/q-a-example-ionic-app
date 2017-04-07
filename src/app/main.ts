021553a8abc6c975f25beb287c5a29236e2d7dcc
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2017-04-07 14:48:14 -0300

    Add some functionalities

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { LoopBackConfig } from '../../sdk';
 
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+LoopBackConfig.setBaseURL('https://q-a-example-loopback-api.herokuapp.com/');
