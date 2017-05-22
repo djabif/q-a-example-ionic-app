3a2ca92af881f2c5a235edb4493c457313034505
Agustin Haller <agustinhaller@gmail.com>
2017-05-22 17:24:18 -0300

    learn-feed styles DONE

--- src/pages/learn-feed/learn-feed.ts
+++ src/pages/learn-feed/learn-feed.ts
@@ -3,11 +3,9 @@ import { NavController, NavParams } from 'ionic-angular';
 import { isPresent } from 'ionic-angular/util/util';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-// import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { LearnDetailsPage } from '../learn-details/learn-details';
 
-// @IonicPage()
 @Component({
   selector: 'learn-feed-page',
   templateUrl: 'learn-feed.html',
